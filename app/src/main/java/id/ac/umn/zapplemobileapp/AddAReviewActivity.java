8d5dc88157cf0b56ff319ce8f7ff46869e9fa1ea
Gracia Angelica N <61301424+grrrracia@users.noreply.github.com>
2021-05-24 12:57:46 +0700

    Update AddAReviewActivity.java

--- app/src/main/java/id/ac/umn/zapplemobileapp/AddAReviewActivity.java
+++ app/src/main/java/id/ac/umn/zapplemobileapp/AddAReviewActivity.java
@@ -5,27 +5,20 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class AddAReviewActivity extends AppCompatActivity {
     ConstraintLayout clUploadImage;
-    TextView tvAddPicture,tvMyRating;
+    ImageView imgReviewImage;
+    TextView tvAddPicture;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_a_review);
 
-        tvMyRating = findViewById(R.id.tvMyRating);
-
-//        Bundle restaurantData = new Bundle();
-//        int restoId = getArguments().getInt("restaurantId");
-//        tvMyRating.setText(restoId);
-
-
         tvAddPicture = findViewById(R.id.tvAddPict);
-//        tvAddPicture.setText(restoId);
-
 
         clUploadImage = findViewById(R.id.reviewImage);
         clUploadImage.setOnClickListener(new View.OnClickListener(){
