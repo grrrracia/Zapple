8a6570812e921386e41d3e3b2e76f176adb50fea
Alexander Moya Hin <Alexander.hin@student.umn.ac.id>
2021-05-31 20:47:23 +0700

    add WriteExternalStorage

--- app/src/main/java/id/ac/umn/zapplemobileapp/MainActivity.java
+++ app/src/main/java/id/ac/umn/zapplemobileapp/MainActivity.java
@@ -70,6 +70,10 @@ public class MainActivity extends AppCompatActivity {
                 requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},1);
                 return;
             }
+            if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},1);
+                return;
+            }
         }
     }
     public void seedIntent(){
